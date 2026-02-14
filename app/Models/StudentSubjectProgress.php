f9d7d63ba1840843dcb43a3b9150a664c8dbbe82
Jerdan Gondayao <jdgonzdayao@gmail.com>
2026-02-15 03:09:01 +0800

    Student Portal: AcademicProgressView

--- app/Models/StudentSubjectProgress.php
+++ app/Models/StudentSubjectProgress.php
@@ -41,23 +41,9 @@ class StudentSubjectProgress extends Model
 
     public function isCompleted()
     {
-        $hasLec = $this->subject->lec_units > 0;
-        $hasLab = $this->subject->lab_units > 0;
-
-        if ($hasLec && $hasLab) {
-            return $this->lecture_completed && $this->laboratory_completed;
+        if ($this->remarks == "completed" || $this->remarks == "failed") {
+            return true;
         }
-
-        if ($hasLec) {
-            return $this->lecture_completed;
-        }
-
-        if ($hasLab) {
-            return $this->laboratory_completed;
-        }
-
-        // Edge case: both units are 0 (shouldn't happen per your store validation, but just in case)
-        return false;
     }
 
     public function has_lec()
